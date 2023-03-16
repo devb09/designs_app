f17839eda0f9dd4d5c4f7a0cc6a606ae3c3dbf19
brn091 <brn091dev@gmail.com>
2023-03-16 17:10:16 -0500

    feat: scroll design page init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:design_app/screens/scroll_deisgn.dart';
 import 'package:flutter/material.dart';
 import 'package:design_app/screens/basic_design.dart';
 
@@ -11,8 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basic_design',
-      routes: {'basic_design': ( _ ) => BasicDesignScreen()},
+      initialRoute: 'scroll_design',
+      routes: {
+        'basic_design': ( _ ) => BasicDesignScreen(),
+        'scroll_design': ( _ ) => ScrollScreen(),
+        },
     );
   }
 }
